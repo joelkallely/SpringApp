d1cfeb8229116916dbd680e48a28691ebc942379
Joel Jose <joel.jose@flytxt.com>
2018-07-17 21:57:34 +0530

    First commit

---
+++ src/main/java/jj/web/App.java
@@ -0,0 +1,25 @@
+package jj.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Hello world!
+ *
+ */
+@RestController
+@EnableAutoConfiguration
+public class App 
+{
+	@RequestMapping("/joel")
+	String joel() {
+		return "joel";
+	}
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+    }
+}
