e6b919a0126900befc832a7dad8c2d4dbcc0869a
joelkallely <joel.kallely95@gmail.com>
2018-07-17 22:51:22 +0530

    Update App.java

--- src/main/java/jj/web/App.java
+++ src/main/java/jj/web/App.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 public class App 
 {
-	@RequestMapping("/")
+	@RequestMapping("/j")
 	String joel() {
 		return "joel";
 	}
